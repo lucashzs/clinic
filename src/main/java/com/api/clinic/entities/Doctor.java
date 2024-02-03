16d20b05f751b1f07a3e6899ae7834af31c3ed52
luquinha <lucassouzafr1@gmail.com>
2024-02-03 14:03:24 -0300

    implementation of CPF and email validation

--- src/main/java/com/api/clinic/entities/Doctor.java
+++ src/main/java/com/api/clinic/entities/Doctor.java
@@ -1,14 +1,15 @@
 package com.api.clinic.entities;
 
-import com.api.clinic.dtos.DoctorDto;
 import com.api.clinic.dtos.RegisterDoctorDto;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -25,10 +26,12 @@ public class Doctor implements UserDetails {
     @NotNull
     @Column(name = "cpf")
     @Id
+    @CPF
     private String document;
 
     @NotBlank
     @NotNull
+    @Email
     private String email;
 
     @NotBlank
